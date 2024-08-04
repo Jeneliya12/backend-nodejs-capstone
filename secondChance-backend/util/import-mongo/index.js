b3aed841d12fba37f4092e1a0c46068e161c250d
Jeneliya Gurung <38620022+Jeneliya12@users.noreply.github.com>
2024-08-03 23:53:22 -0400

    Update index.js

--- secondChance-backend/util/import-mongo/index.js
+++ secondChance-backend/util/import-mongo/index.js
@@ -1,50 +1,15 @@
-require('dotenv').config();
-const MongoClient = require('mongodb').MongoClient;
-const fs = require('fs');
+const express = require('express');
+const path = require('path');
+const app = express();
 
-// MongoDB connection URL with authentication options
-let url = `${process.env.MONGO_URL}`;
-let filename = `${__dirname}/secondChanceItems.json`;
-const dbName = 'secondChance';
-const collectionName = 'secondChanceItems';
+app.use(express.static(path.join(__dirname, 'build')));
 
-// notice you have to load the array of items into the data object
-const data = JSON.parse(fs.readFileSync(filename, 'utf8')).docs;
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
-// connect to database and insert data into the collection
-async function loadData() {
-    const client = new MongoClient(url);
+app.get('/app', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
-    try {
-        // Connect to the MongoDB client
-        await client.connect();
-        console.log("Connected successfully to server");
-
-        // database will be created if it does not exist
-        const db = client.db(dbName);
-
-        // collection will be created if it does not exist
-        const collection = db.collection(collectionName);
-        let cursor = await collection.find({});
-        let documents = await cursor.toArray();
-
-        if(documents.length == 0) {
-            // Insert data into the collection
-            const insertResult = await collection.insertMany(data);
-            console.log('Inserted documents:', insertResult.insertedCount);
-        } else {
-            console.log("Items already exists in DB")
-        }
-    } catch (err) {
-        console.error(err);
-    } finally {
-        // Close the connection
-        await client.close();
-    }
-}
-
-loadData();
-
-module.exports = {
-    loadData,
-  };
+app.listen(9000);
